627e903b022ef10c9e2c99889723c8970554c0d3
Maarten Dekker <maarten.dekker@capgemini.com>
2024-09-12 14:31:06 +0200

    optimize metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,14 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export async function generateMetadata(){
+  const posts = await getPosts();
+  return {
+    title: `Browse our ${posts.length} posts`,
+    description: 'Bowse all our posts'
+  }
+};
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
